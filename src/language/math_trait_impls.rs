e6ea7ee9f03b7908e64b08e22900ec2dcfaf9440
Jordan <46637683+JordanllHarper@users.noreply.github.com>
2025-04-06 12:25:38 +0100

    fix: divide should always give a float

--- src/language/math_trait_impls.rs
+++ src/language/math_trait_impls.rs
@@ -67,7 +67,9 @@ impl Div for NumericLiteral {
 
     fn div(self, rhs: Self) -> Self::Output {
         match (self, rhs) {
-            (NumericLiteral::Int(i1), NumericLiteral::Int(i2)) => NumericLiteral::Int(i1 / i2),
+            (NumericLiteral::Int(i1), NumericLiteral::Int(i2)) => {
+                NumericLiteral::Float(i1 as f64 / i2 as f64)
+            }
             (NumericLiteral::Int(i1), NumericLiteral::Float(f1)) => {
                 NumericLiteral::Float(i1 as f64 / f1)
             }
