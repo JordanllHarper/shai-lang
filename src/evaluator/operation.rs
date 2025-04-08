e0af39f76f4e784c217224a362b463fd867d65a0
JordanH <jordanh@apadmi.com>
2025-04-08 10:40:33 +0100

    fix(evaluation): rework evaluation to use values instead of expressions
    Allows for more concise matching and handling

--- src/evaluator/operation.rs
+++ src/evaluator/operation.rs
@@ -1,3 +1,5 @@
+use std::fmt::format;
+
 use super::environment::*;
 use crate::evaluator::*;
 use crate::language::*;
@@ -6,97 +8,91 @@ pub fn evaluate_operation(
     state: EnvironmentState,
     m: Operations,
 ) -> (EnvironmentState, Result<EnvironmentBinding, EvaluatorError>) {
-    let lh_binding = get_binding_from_expression(&state, *m.lhs);
-    let rh_binding = get_binding_from_expression(&state, *m.rhs);
-    let bindings = match (lh_binding, rh_binding) {
-        (Ok(b1), Ok(b2)) => (b1, b2),
-        (_, Err(e)) | (Err(e), _) => return (state, Err(e)),
-    };
-
-    let (state, left_value) = get_values_from_binding(state, bindings.0);
-    let (state, right_value) = get_values_from_binding(state, bindings.1);
-    let values = match (left_value, right_value) {
-        (Ok(left_value), Ok(right_value)) => (left_value, right_value),
-        (_, Err(e)) | (Err(e), _) => return (state, Err(e)),
-    };
-    let value_literals = match values {
-        (Value::ValueLiteral(v1), Value::ValueLiteral(v2)) => (v1, v2),
-        (_, Value::Void) | (Value::Void, _) => {
-            return (state, Err(EvaluatorError::InvalidOperationValue))
-        }
-    };
-
+    let sides = (*m.lhs, *m.rhs);
     match m.operation {
-        Operator::Add => handle_add(state, value_literals),
-        Operator::Subtract => handle_subtract(state, value_literals),
-        Operator::Multiply => handle_multiply(state, value_literals),
-        Operator::Divide => handle_divide(state, value_literals),
+        Operator::Add => handle_add(state, sides),
+        Operator::Subtract => handle_subtract(state, sides),
+        Operator::Multiply => handle_multiply(state, sides),
+        Operator::Divide => handle_divide(state, sides),
     }
 }
 
 fn handle_concatenation(
     state: EnvironmentState,
     c: CharacterBasedLiteral,
-    other: ValueLiteral,
-    in_reverse: bool,
+    expr: Expression,
+    reverse: bool,
 ) -> (EnvironmentState, Result<EnvironmentBinding, EvaluatorError>) {
-    let s = other.to_string();
+    let s = match get_binding_from_expression(&state, expr) {
+        Ok(b) => match get_string_from_binding(&state, &b) {
+            Ok(s) => s,
+            Err(e) => return (state, Err(e)),
+        },
+        Err(e) => return (state, Err(e)),
+    };
 
-    let format = if in_reverse {
-        format!("{}{}", s, c)
-    } else {
+    let format = if reverse {
         format!("{}{}", c, s)
+    } else {
+        format!("{}{}", s, c)
     };
     (state, Ok(EnvironmentBinding::new_string(&(format))))
 }
 
 fn handle_add(
     state: EnvironmentState,
-    values: (ValueLiteral, ValueLiteral),
+    sides: (Expression, Expression),
 ) -> (EnvironmentState, Result<EnvironmentBinding, EvaluatorError>) {
-    match values {
-        (ValueLiteral::CharacterBased(c), other) => handle_concatenation(state, c, other, false),
-        (other, ValueLiteral::CharacterBased(c)) => handle_concatenation(state, c, other, true),
-        (ValueLiteral::Numeric(n1), ValueLiteral::Numeric(n2)) => {
-            (state, Ok(EnvironmentBinding::new_numeric(n1 + n2)))
+    match sides {
+        (
+            Expression::ValueLiteral(ValueLiteral::Numeric(n1)),
+            Expression::ValueLiteral(ValueLiteral::Numeric(n2)),
+        ) => (state, Ok(EnvironmentBinding::new_numeric(n1 + n2))),
+        (Expression::ValueLiteral(ValueLiteral::CharacterBased(c)), other) => {
+            handle_concatenation(state, c, other, true)
         }
-        _ => (state, Err(EvaluatorError::InvalidOperationValue)),
+        (other, Expression::ValueLiteral(ValueLiteral::CharacterBased(c))) => {
+            handle_concatenation(state, c, other, false)
+        }
+        _ => (state, Err(EvaluatorError::InvalidAddition)),
     }
 }
 
 fn handle_subtract(
     state: EnvironmentState,
-    values: (ValueLiteral, ValueLiteral),
+    sides: (Expression, Expression),
 ) -> (EnvironmentState, Result<EnvironmentBinding, EvaluatorError>) {
-    match values {
-        (ValueLiteral::Numeric(n1), ValueLiteral::Numeric(n2)) => {
-            (state, Ok(EnvironmentBinding::new_numeric(n1 - n2)))
-        }
-        _ => (state, Err(EvaluatorError::InvalidOperationValue)),
+    match sides {
+        (
+            Expression::ValueLiteral(ValueLiteral::Numeric(n1)),
+            Expression::ValueLiteral(ValueLiteral::Numeric(n2)),
+        ) => (state, Ok(EnvironmentBinding::new_numeric(n1 - n2))),
+        _ => (state, Err(EvaluatorError::InvalidSubtract)),
     }
 }
 
 fn handle_divide(
     state: EnvironmentState,
-    values: (ValueLiteral, ValueLiteral),
+    sides: (Expression, Expression),
 ) -> (EnvironmentState, Result<EnvironmentBinding, EvaluatorError>) {
-    match values {
-        (ValueLiteral::Numeric(n1), ValueLiteral::Numeric(n2)) => {
-            (state, Ok(EnvironmentBinding::new_numeric(n1 / n2)))
-        }
-
-        _ => (state, Err(EvaluatorError::InvalidOperationValue)),
+    match sides {
+        (
+            Expression::ValueLiteral(ValueLiteral::Numeric(n1)),
+            Expression::ValueLiteral(ValueLiteral::Numeric(n2)),
+        ) => (state, Ok(EnvironmentBinding::new_numeric(n1 / n2))),
+        _ => (state, Err(EvaluatorError::InvalidDivide)),
     }
 }
 
 fn handle_multiply(
     state: EnvironmentState,
-    values: (ValueLiteral, ValueLiteral),
+    sides: (Expression, Expression),
 ) -> (EnvironmentState, Result<EnvironmentBinding, EvaluatorError>) {
-    match values {
-        (ValueLiteral::Numeric(n1), ValueLiteral::Numeric(n2)) => {
-            (state, Ok(EnvironmentBinding::new_numeric(n1 * n2)))
-        }
-        _ => (state, Err(EvaluatorError::InvalidOperationValue)),
+    match sides {
+        (
+            Expression::ValueLiteral(ValueLiteral::Numeric(n1)),
+            Expression::ValueLiteral(ValueLiteral::Numeric(n2)),
+        ) => (state, Ok(EnvironmentBinding::new_numeric(n1 * n2))),
+        _ => (state, Err(EvaluatorError::InvalidMultiplication)),
     }
 }
