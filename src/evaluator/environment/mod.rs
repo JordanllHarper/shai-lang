42369eaec79783389c9cbe494a2d48324cf9c94e
JordanH <jordanh@apadmi.com>
2025-04-27 14:37:25 +0100

    style(environment): move implementation blocks to bottom of file

--- src/evaluator/environment/mod.rs
+++ src/evaluator/environment/mod.rs
@@ -16,45 +16,6 @@ pub struct EnvironmentState {
     pub std_lib_symbols: HashMap<String, RustBinding>,
 }
 
-impl EnvironmentState {
-    pub fn new(local_symbols: HashMap<String, EnvironmentBinding>) -> Self {
-        Self {
-            current_scope: Scope::new_with_symbols(local_symbols, None),
-            std_lib_symbols: HashMap::from([
-                ("print".to_string(), RustBinding::Print(std_rust_print)),
-                ("len".to_string(), RustBinding::Len),
-                ("append".to_string(), RustBinding::Append),
-            ]),
-        }
-    }
-
-    pub fn get_local_binding(&self, symbol: &str) -> Option<EnvironmentBinding> {
-        get_local_binding_recursive(symbol, &self.current_scope)
-    }
-
-    pub fn add_or_mutate_symbols(
-        &mut self,
-        symbol: &str,
-        binding: EnvironmentBinding,
-    ) -> Result<EnvironmentBinding, EvaluatorError> {
-        mutate_local_binding_recursive(&mut self.current_scope, symbol, binding)
-    }
-    pub fn push_scope(&mut self) -> Self {
-        let current_scope = self.current_scope.clone();
-        let new = Scope::new(Some(current_scope));
-        self.current_scope = new;
-        self.to_owned()
-    }
-    pub fn pop_scope(&mut self) -> Self {
-        if let Some(parent) = self.current_scope.parent.clone() {
-            self.current_scope = *parent;
-            self.to_owned()
-        } else {
-            panic!("Attempted to pop top level scope")
-        }
-    }
-}
-
 /// The user's local scope, where variables will be looked up and mutated accordingly.
 ///
 /// Mutations start at the child level and move up to the parent
@@ -62,25 +23,6 @@ impl EnvironmentState {
 pub struct Scope {
     pub local_symbols: HashMap<String, EnvironmentBinding>,
     pub parent: Option<Box<Scope>>,
-}
-
-impl Scope {
-    pub fn new(parent: Option<Scope>) -> Self {
-        Self {
-            local_symbols: HashMap::new(),
-            parent: parent.map(Box::new),
-        }
-    }
-
-    pub fn new_with_symbols(
-        local_symbols: HashMap<String, EnvironmentBinding>,
-        parent: Option<Scope>,
-    ) -> Self {
-        Self {
-            local_symbols,
-            parent: parent.map(Box::new),
-        }
-    }
 }
 
 /// A binding in the Environment. This represents all possible values a symbol can resolve to.
@@ -120,4 +62,62 @@ pub struct RangeValue {
 pub enum MutateBindingError {
     InvalidRedeclaration,
     NoIdentifier,
+}
+
+impl EnvironmentState {
+    pub fn new(local_symbols: HashMap<String, EnvironmentBinding>) -> Self {
+        Self {
+            current_scope: Scope::new_with_symbols(local_symbols, None),
+            std_lib_symbols: HashMap::from([
+                ("print".to_string(), RustBinding::Print(std_rust_print)),
+                ("len".to_string(), RustBinding::Len),
+                ("append".to_string(), RustBinding::Append),
+            ]),
+        }
+    }
+
+    pub fn get_local_binding(&self, symbol: &str) -> Option<EnvironmentBinding> {
+        get_local_binding_recursive(symbol, &self.current_scope)
+    }
+
+    pub fn add_or_mutate_symbols(
+        &mut self,
+        symbol: &str,
+        binding: EnvironmentBinding,
+    ) -> Result<EnvironmentBinding, EvaluatorError> {
+        mutate_local_binding_recursive(&mut self.current_scope, symbol, binding)
+    }
+    pub fn push_scope(&mut self) -> Self {
+        let current_scope = self.current_scope.clone();
+        let new = Scope::new(Some(current_scope));
+        self.current_scope = new;
+        self.to_owned()
+    }
+    pub fn pop_scope(&mut self) -> Self {
+        if let Some(parent) = self.current_scope.parent.clone() {
+            self.current_scope = *parent;
+            self.to_owned()
+        } else {
+            panic!("Attempted to pop top level scope")
+        }
+    }
+}
+
+impl Scope {
+    pub fn new(parent: Option<Scope>) -> Self {
+        Self {
+            local_symbols: HashMap::new(),
+            parent: parent.map(Box::new),
+        }
+    }
+
+    pub fn new_with_symbols(
+        local_symbols: HashMap<String, EnvironmentBinding>,
+        parent: Option<Scope>,
+    ) -> Self {
+        Self {
+            local_symbols,
+            parent: parent.map(Box::new),
+        }
+    }
 }
